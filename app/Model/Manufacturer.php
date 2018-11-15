906e1cfb48f8d11b433f4fe016602cfe863f090a
Noha Drweesh <noha.drweesh@robovics.com>
2018-11-15 19:23:18 +0200

    fix typo

--- app/Model/Manufacturer.php
+++ app/Model/Manufacturer.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Manufacturer extends Model
 {
     //
-    protected $table='manufacturers ';
+    protected $table='manufacturers';
     protected $fillable=['name_ar','name_en','mobile','email','twitter','facebook','website','contact_name','lat','lang','logo'];
 }
--- resources/views/admin/manufacturers/create.blade.php
+++ resources/views/admin/manufacturers/create.blade.php
@@ -1,12 +1,16 @@
 @extends('admin.index')
 @section('content')
+@push('js')
+<script type="text/javascript" src='http://maps.google.com/maps/api/js?sensor=false&libraries=places'></script>
+<script type="text/javascript" src="{{url('design\adminlte\dist\js\locationpicker.jquery.js')}}"></script>
+@endpush
 
 <div class="box">
   <div class="box-header">
     <h3 class="box-title">{{$title}}</h3>
   </div>
   <!-- /.box-header -->
-  <div class="box-body">
+     <div class="box-body">
   	{!! Form::open(['id'=>'form_data','url'=>admin_url('manufacturers'),'method'=>'Post','files'=>true])!!}
         <div class="form-group">
              {!!  Form::label('name_ar','Arabic  Name')!!}
@@ -23,6 +27,25 @@
       <div class="form-group">
           {!!  Form::label('email','email')!!}
           {!! Form::email('email',old('email'),['class'=>'form-control'])!!}
+      </div>
+      <div class="form-group">
+          {!!  Form::label('contact_name','contact_name')!!}
+          {!! Form::text('contact_name',old('contact_name'),['class'=>'form-control'])!!}
+      </div>
+
+      <div class="form-group">
+          {!!  Form::label('twitter','twitter')!!}
+          {!! Form::url('twitter',old('twitter'),['class'=>'form-control'])!!}
+      </div>
+
+      <div class="form-group">
+          {!!  Form::label('facebook','facebook')!!}
+          {!! Form::url('facebook',old('facebook'),['class'=>'form-control'])!!}
+      </div>
+
+      <div class="form-group">
+          {!!  Form::label('website','website')!!}
+          {!! Form::url('website',old('website'),['class'=>'form-control'])!!}
       </div>
 
         <div class="form-group">
